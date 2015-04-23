ec209f927b9a3e26971409ec60f2470e190aed00
Mathieu Fallows <mathieufallows@gmail.com>
2015-04-22 19:22:27 -0700

    added users model functionality

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,14 @@ Rails.application.routes.draw do
   put "features/:id" => "features#update"
   delete "features/:id" => "features#destroy"
 
+  get "users" => "users#index" 
+  get "signup" => "users#new" 
+  post "users" => "users#create"
 
+  #routes for logging in/out
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
