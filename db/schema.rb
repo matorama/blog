1a833ccb3b48f043c22a7c727c7239e4debf1178
Mathieu Fallows <mathieufallows@gmail.com>
2015-04-22 17:29:45 -0700

    dropped excess tables and added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422044823) do
+ActiveRecord::Schema.define(version: 20150423002519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,33 +33,12 @@ ActiveRecord::Schema.define(version: 20150422044823) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
-
-  create_table "profiles", force: :cascade do |t|
-    t.string   "full_name"
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "features"
-  add_foreign_key "posts", "users"
-  add_foreign_key "profiles", "users"
 end
