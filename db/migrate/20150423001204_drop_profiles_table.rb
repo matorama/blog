9a0980761fcfb03967a94f023d10c738244df2d1
Mathieu Fallows <mathieufallows@gmail.com>
2015-04-23 21:03:36 -0700

    blaise deletes migrations

--- db/migrate/20150423001204_drop_profiles_table.rb
+++
@@ -1,5 +0,0 @@
-class DropProfilesTable < ActiveRecord::Migration
-  def change
-  	drop_table :profiles
-  end
-end
--- db/migrate/20150423001538_drop_roles_users.rb
+++
@@ -1,5 +0,0 @@
-class DropRolesUsers < ActiveRecord::Migration
-  def change
-  	drop_table :roles_users
-  end
-end
